a5b1faf2b89440f78bf44c4edb1b67c16a88889b
linghuchong <280797259@qq.com>
2021-01-15 21:12:41 +0800

    增加夺命剑

---
+++ Duoming.java
@@ -0,0 +1,9 @@
+package com.atguigu.huashanjianfa;
+
+public class Duoming {
+    public static void main(String[] args) {
+        System.out.println("duming");
+        System.out.println("duming");
+        System.out.println("duming");
+    }
+}
